8e88d6e6161319f6e84e5cb75dbc801b0e5d5fc6
Sven Painer <sven@painer.de>
2022-07-29 17:48:17 +0200

    Project setup.

---
+++ src/main/java/de/painer/homecockpit/HomeCockpit.java
@@ -0,0 +1,12 @@
+package de.painer.homecockpit;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class HomeCockpit {
+
+    public static void main(String[] args) {
+        log.debug("HELLO WORLD");
+    }
+
+}
